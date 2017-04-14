0db8d388b2ec169cd6835384973c1ecb26e77bd0
Kevin Jennison <kevin.jennison1@gmail.com>
2017-04-13 20:21:09 -0700

    Local version of ad caching, prefetching, and expiration.

--- main.js
+++ main.js
@@ -3,38 +3,129 @@
   // Set on initialization.
   var config;
 
-  function mockAdResponse() {
-    return {
-      ads: {
-        'div-gpt-ad-1464385742501-0': {
-          ad: '<div><img src="http://goinkscape.com/wp-content/uploads/2015/07/leaderboard-ad-final.png"</img></div>',
-        },
-        'div-gpt-ad-1464385677836-0': {
-          ad: '<div><img src="http://www.guardyourhealth.com/wp-content/uploads/2013/07/300x250_GYH_WebBanner.jpg"</img></div>',
-        },
-      }
+  // Length of time we can use an ad after we preload it.
+  var AD_EXPIRATION_SECONDS = 10;
+
+  // Simple interface to localStorage.
+  var store = {
+    _get: function(key) {
+      return JSON.parse(localStorage.getItem(key));
+    },
+
+    _set: function(key, val) {
+      localStorage.setItem(key, JSON.stringify(val));
+    },
+
+    _cacheKey: 'leopard_ad_cache',
+
+    getCachedAds: function() {
+      return this._get(this._cacheKey);
+    },
+
+    setCachedAds: function(adData) {
+      return this._set(this._cacheKey, adData);
+    },
+  };
+
+  function formatAdData(adResponse) {
+    var formattedAds = {};
+    for (var slotId in adResponse.ads) {
+      var adData = adResponse.ads[slotId];
+      formattedAds[slotId] = {
+        ad: adData.ad,
+        fetchedDate: new Date(),
+        slotId: slotId,
+      };
     }
+    return formattedAds;
   }
 
-  function renderAds(adResponse) {
-    if (!adResponse || !adResponse.ads) {
-      console.log('No ad responses.');
+  // Return the ad if one exists in the cache and hasn't expired;
+  // otherwise, return null.
+  function getAdFromCache(slotId) {
+    var cachedAds = store.getCachedAds();
+    if (!cachedAds || !cachedAds[slotId]) {
+      console.log('No matching ad in cache for placement ' + slotId + '.');
       return;
     }
-    for (var slotId in adResponse.ads) {
-      var container = document.getElementById(slotId);
-      container.innerHTML = adResponse.ads[slotId].ad;
+
+    var ad = cachedAds[slotId];
+    var adFetchedTime = (new Date(ad.fetchedDate)).getTime();
+    var secondsAgoAdFetched = ((new Date()).getTime() - adFetchedTime) / 1000;
+    if (secondsAgoAdFetched > AD_EXPIRATION_SECONDS) {
+      console.log('Ad in cache is expired for placement ' + slotId + '.');
+      return;
     }
+
+    console.log('Found cached ad for placement ' + slotId + '.');
+
+    // Delete ad from the cache.
+    delete cachedAds.slotId;
+    store.setCachedAds(cachedAds);
+
+    return ad;
+  }
+
+  function mockAdResponse(adUnits) {
+
+    var nowStr = new Date().toTimeString().split(" ")[0];
+    var ads = {
+      'div-gpt-ad-1464385742501-0': {
+        ad: '<div><div class="fetched-at">Fetched at: ' + nowStr + '</div><img src="http://goinkscape.com/wp-content/uploads/2015/07/leaderboard-ad-final.png"</img></div>',
+      },
+      'div-gpt-ad-1464385677836-0': {
+        ad: '<div><div class="fetched-at">Fetched at: ' + nowStr + '</div><img src="http://www.guardyourhealth.com/wp-content/uploads/2013/07/300x250_GYH_WebBanner.jpg"</img></div>',
+      },
+    };
+
+    var adResponse = {
+      ads: {},
+    };
+    adUnits.forEach(function(adUnit) {
+      adResponse.ads[adUnit.code] = ads[adUnit.code];
+    });
+    return adResponse;
+  }
+
+  function fetchAdsFromRemote(adUnits) {
+    var adResponse = mockAdResponse(adUnits);
+    var ads = formatAdData(adResponse);
+    return ads;
+  }
+
+  // adData is the object of formatted ad data.
+  function renderAd(adData) {
+    var container = document.getElementById(adData.slotId);
+    container.innerHTML = adData.ad;
   }
 
   function fetchAds() {
-
-    // TODO: check if any matching ads exist in cache before fetching
-    // from remote.
     console.log('Fetching ads.');
 
-    var adResponse = mockAdResponse();
-    renderAds(adResponse);
+    var adUnitsToFetch = [];
+
+    // Check if any matching ads exist in cache before fetching from remote.
+    config.adUnits.forEach(function(adUnit) {
+      var cachedAd = getAdFromCache(adUnit.code);
+      if (cachedAd) {
+        renderAd(cachedAd);
+      } else {
+        adUnitsToFetch.push(adUnit);
+      }
+    });
+    
+    if (adUnitsToFetch.length) {
+      console.log('Fetching fresh ads for uncached placements.');
+    }
+    var ads = fetchAdsFromRemote(adUnitsToFetch);
+    for (var key in ads) {
+      renderAd(ads[key]);
+    }
+
+    // Preload all ads for next time.
+    console.log('Preloading ads for all placements.');
+    var preloadedAds = fetchAdsFromRemote(config.adUnits);
+    store.setCachedAds(preloadedAds);
   }
 
   function main() {
